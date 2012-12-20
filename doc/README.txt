2b11a804b36ffbd0dc2019e48586acb6941595ee
tverhoken <thomas.verhoken@gmail.com>
2012-12-20 19:21:59 +0100

    ajout de la doc

---
+++ doc/README.txt
@@ -0,0 +1,10 @@
+Voici les étapes à suivre afin de faire fonctionner le projet :
+
+1 - Importer le projet
+
+2 - Lancer le projet en mode serveur
+
+3 - Dans le dossier "client" du projet se trouve un fichier "testIntegration.html".
+Ce fichier peut se trouver n'importe où sur votre ordinateur et même sur votre réseau.
+Il fonctionnera tout de même (à noter que si vous le placez sur une autre machine de votre réseau,
+Il vous faudra alors remplacer "localhost" par le nom de votre machine sur le réseau).
--- webapp/js/welcomeService.js
+++ webapp/js/welcomeService.js
@@ -1,8 +1,20 @@
 var requete;
 
+/**
+ * Cette methode recupere la valeur de l'element <b>toCheck</b>, l'envoi au
+ * service et appel la fonction qui va mettre a jour l'IHM au niveau de
+ * l'element <b>divToFill</b> une fois la reponse du service recue.
+ * 
+ * @param divToFill
+ *            l'element de type <i>div</i> qui va recevoir la reponse de
+ *            l'appel au service.
+ * @param toCheck
+ *            l'element de type <i>input</i> que l'on va inspecter.
+ */
 function callWelcomeService(divToFill, toCheck) {
 	var name = document.getElementById(toCheck).value;
-	var url = "http://localhost:8080/WelcomeService/welcomeService?name=" + name;
+	var url = "http://localhost:8080/WelcomeService/welcomeService?name="
+			+ name;
 	if (window.XMLHttpRequest) {
 		requete = new XMLHttpRequest();
 		requete.open("GET", url, true);
@@ -20,6 +32,12 @@ function callWelcomeService(divToFill, toCheck) {
 	}
 }
 
+/**
+ * Methode qui va mettre a jour la partie IHM une fois la reponse du service
+ * recue.
+ * 
+ * @param divToFill
+ */
 function updateIHM(divToFill) {
 	document.getElementById(divToFill).innerHTML = requete.responseText;
 }
